d44a941aa472c5e2d7a1e6c7734c641600c53439
DevYouriD <youri.dera@gmail.com>
2026-04-12 00:50:55 +0200

    Set favicon

--- savory-frontend/app/layout.tsx
+++ savory-frontend/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 
     return (
         <html lang="en" className={cn("font-mono", jetbrainsMono.variable)}>
-        <link rel="icon" href="/chicken.svg" />
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex`}>
         {mounted && (
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
